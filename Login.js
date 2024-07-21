9689a8ff18fd8bdb3b2d369d427f32835cfaa564
JorgeIgnacioCondoriAguirre <jcondoriaguirre@frba.utn.edu.ar>
2024-07-20 22:37:59 -0300

    API Rest

---
+++ Login.js
@@ -0,0 +1,59 @@
+/*alert,console.log
+var y=15;
+var z=15;*/
+/*var x = y+z;
+document.write(x);
+let nombre = prompt('¿What is your name?');
+document.write(`Welcome ${nombre}`);
+if(nombre=`jorge`){
+    let num1 = prompt(`Give me a number`);
+    let num2 = prompt(`Give me  other number`);
+    alert(`The sum of the numbers is ${(parseInt(num1)+parseInt(num2))}`);
+}
+
+//funcion autoejecutable moderna
+const funcion = ((nombre) => {
+    document.write(`Hola ${nombre}\n`)
+})('Jorge')
+
+//funcion autoejecutable antigua
+const funcion2 = (function (nombre) {
+    document.write(`${nombre}\n`)
+})('Ignacio')
+
+    //funcion autoejecutable anonima
+    (function (nombre) {
+        document.write(`${nombre}\n`)
+    }('Condori'))
+
+    //funcion autoejecutable moderna anonima
+    ((nombre) => {
+        document.write(`${nombre}`)
+    })('Aguirre')
+
+//fomulario
+
+const valueName = (nameValue) => {
+ 
+}
+const valueLastname = (lastnameValue) => {
+
+}
+const valueEmail = (emailValue) => {
+
+}
+const valuePassword = (passwordValue) => {
+
+}
+const name = document.querySelector('#name')
+const lastname = document.querySelector('#lastname')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+
+document.addEventListener('change', (e) => {
+    e.target.matches('#name') ? valueName(e.target.value) : null
+    e.target.matches('#lastname') ? valueLastname(e.target.value) : null
+    e.target.matches('#email') ? valueEmail(e.target.value) : null
+    e.target.matches('#password') ? valuePassword(e.target.value) : null
+})
+*/
---
+++ index.js
@@ -0,0 +1,53 @@
+const mysql = require("mysql");
+const express = require("express");
+const app = express();
+
+//Creamos conexion con mysql
+let conexion = mysql.createConnection({
+    host: "localhost",
+    database: "users",
+    user: "root",
+    password: "43988157"
+});
+
+//Motor de vistas para conectar el html con javascript
+app.set("view engine", "ejs");
+
+//Para que reconozca el html como objetos
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//Crear ruta con el html
+app.get("/", function(answer,request){
+    request.render("login");
+});
+
+//Guardamos los datos del html
+app.post("/value", function(answer,request){
+    const data = answer.body;
+
+    let email = data.email;
+    let password = data.password;
+
+    //Guardar los datos en la base de datos
+    let insertar = "INSERT INTO user (email,password) VALUES ('"+email+"','"+password+"')";
+
+    conexion.query(insertar, function(error){
+        if(error) throw error;
+        console.log("Datos cargados correctamente");
+    });
+});
+
+
+
+
+
+//Creacion de servidor local en puerto 3000
+app.listen(3000,function(){
+    console.log("Servidor funcionando");
+});
+
+
+
+
+
